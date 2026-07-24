403af663e90021b10498a50248968e7240fdfb48
Ritik Kaushik <kaushikritik050@gmail.com>
2026-07-24 17:55:22 +0530

    Time: 4 ms (37.65%), Space: 43.7 MB (7.59%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('||s.charAt(i)=='[' ||s.charAt(i)=='{'){
+                st.push(s.charAt(i));
+            }else{
+                if(! st.empty()){
+                    if(st.peek()=='(' && s.charAt(i)==')'){
+                        st.pop();
+                    } else if(st.peek()=='['&& s.charAt(i)==']'){
+                        st.pop();
+                    } else if(st.peek()=='{'&& s.charAt(i)=='}'){
+                        st.pop();
+                    } 
+                    else{
+                    return false;
+                } 
+                }else{
+                    return false;
+                } 
+            }
+        }
+        if(st.empty()){
+            return true;
+        }
+        return false;
+    }
+}
