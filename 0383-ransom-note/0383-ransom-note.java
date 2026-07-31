21c807d7b281c8a3e1a369b832e1ed67c8a4d3a9
Ritik Kaushik <kaushikritik050@gmail.com>
2026-07-31 11:22:35 +0530

    Time: 4 ms (47.6%), Space: 46.8 MB (41.84%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,29 +1,23 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character,Integer>map1=new HashMap<>();
-         HashMap<Character,Integer>map2=new HashMap<>();
-         for(int i=0;i<ransomNote.length();i++){
-            map1.put(ransomNote.charAt(i),map1.getOrDefault(ransomNote.charAt(i),0)+1);
-         }
-         for(int i=0;i<magazine.length();i++){
-             map2.put(magazine.charAt(i),map2.getOrDefault(magazine.charAt(i),0)+1);
-         }
-              return  compare(map1,map2);
-          
-
-    }
-    public boolean compare(HashMap<Character,Integer> map1, HashMap<Character,Integer> map2) {
-
-    for (Map.Entry<Character, Integer> entry : map1.entrySet()) {
-        char c = entry.getKey();
-        int fNeed = entry.getValue();
-        int fHave = map2.getOrDefault(c, 0);
-
-        if (fHave < fNeed) {
-            return false;
+        HashMap<Character,Integer>map=new HashMap<>();
+        for(int i=0;i<ransomNote.length();i++){
+            map.put(ransomNote.charAt(i),map.getOrDefault(ransomNote.charAt(i),0)+1);
         }
+        for(int i=0;i<magazine.length();i++){
+            if(map.containsKey(magazine.charAt(i))){
+                map.put(magazine.charAt(i),map.getOrDefault(magazine.charAt(i),0)-1);
+                if(map.get(magazine.charAt(i))==0){
+                    map.remove(magazine.charAt(i));
+                }
+            }else{
+                continue;
+            }
+            if(map.isEmpty()){
+                return true;
+            }
+        }
+        return false;
+        
     }
-    return true;
 }
-    
- }
